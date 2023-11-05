b0c39be3b4550a715681cb5bb54b991270c0c4fc
Michael McGrath <mcgramic@amazon.com>
2023-11-05 16:30:23 -0700

    Adding new UI.

--- yew-app/src/main.rs
+++ yew-app/src/main.rs
@@ -1,6 +1,5 @@
-use gloo::console;
-use js_sys::Date;
 use yew::{html, Component, Context, Html};
+use std::cmp;
 
 // Define the possible messages which can be sent to the component
 pub enum Msg {
@@ -12,7 +11,9 @@ pub enum Plyr {
     Table,
     P1,
     P2,
-    Dead
+    Dead,
+    Inning,
+    Rack
 }
 
 pub struct GameAction {
@@ -20,61 +21,86 @@ pub struct GameAction {
     action: Msg
 }
 
+pub struct Rack {
+    player1: i64,
+    player2: i64,
+    dead: i64,
+    innings: i64,
+}
+
 pub struct App {
-    table: i64, // This will store the counter value
-    player1: i64, // This will store the counter value
-    player2: i64, // This will store the counter value
-    dead: i64, // This will store the counter value
+    rack: usize,
+    totals: Rack,
+    racks: Vec<Rack>
 }
 
 impl Component for App {
     type Message = GameAction;
-    // type Player = Plyr;
-    // type PlayerAction = GameAction;
     type Properties = ();
 
     fn create(_ctx: &Context<Self>) -> Self {
-        Self { table: 9, player1: 0, player2: 0, dead: 0 }
+        let mut racks = Vec::with_capacity(300);
+        for i in 0..300 {
+            racks.push(Rack{
+                player1: 0,
+                player2: 0,
+                dead: 0,
+                innings: 0,
+            });
+        }
+        Self { rack: 0, racks, totals: Rack {
+            player1: 0,
+            player2: 0,
+            dead: 0,
+            innings: 0,
+        } }
+
+
     }
 
     fn update(&mut self, _ctx: &Context<Self>, action: Self::Message) -> bool {
+        let current_rack = &mut self.racks[self.rack];
         match action {
-            GameAction{entity: Plyr::Table, action: Msg::Increment} => {
-                self.table += 1;
-                return true;
-            }
-            GameAction{entity: Plyr::Table, action: Msg::Decrement} => {
-                self.table -= 1;
-                return true;
-            }
             GameAction{entity: Plyr::P1, action: Msg::Increment} => {
-                self.player1 += 1;
-                self.table -=1;
+                current_rack.player1 += 1;
                 return true;
             }
             GameAction{entity: Plyr::P1, action: Msg::Decrement} => {
-                self.player1 -= 1;
-                self.table += 1;
+                current_rack.player1 -= 1;
                 return true;
             }
             GameAction{entity: Plyr::P2, action: Msg::Increment} => {
-                self.player2 += 1;
-                self.table -= 1;
+                current_rack.player2 += 1;
                 return true;
             }
             GameAction{entity: Plyr::P2, action: Msg::Decrement} => {
-                self.player2 -= 1;
-                self.table += 1;
+                current_rack.player2 -= 1;
                 return true;
             }
             GameAction{entity: Plyr::Dead, action: Msg::Increment} => {
-                self.dead += 1;
-                self.table -= 1;
+                current_rack.dead += 1;
                 return true;
             }
             GameAction{entity: Plyr::Dead, action: Msg::Decrement} => {
-                self.dead -= 1;
-                self.table += 1;
+                current_rack.dead -= 1;
+                return true;
+            }
+            GameAction{entity: Plyr::Inning, action: Msg::Increment} => {
+                current_rack.innings += 1;
+                return true;
+            }
+            GameAction{entity: Plyr::Inning, action: Msg::Decrement} => {
+                current_rack.innings -= 1;
+                return true;
+            }
+            GameAction{entity: Plyr::Rack, action: Msg::Increment} => {
+                self.rack += 1;
+                self.totals = update_totals(&self.racks);
+                return true;
+            }
+            GameAction{entity: Plyr::Rack, action: Msg::Decrement} => {
+                self.rack = cmp::max(0, self.rack -1);
+                self.totals = update_totals(&self.racks);
                 return true;
             }
             _ => { false }
@@ -84,68 +110,104 @@ impl Component for App {
     fn view(&self, ctx: &Context<Self>) -> Html {
         html! {
             <div>
-                <div class="panel">
-                    // // A button to send the Increment message
-                    // <button class="button" onclick={ctx.link().callback(|_| GameAction{entity: Plyr::Table, action: Msg::Increment})}>
-                    //     { "+1 to table" }
-                    // </button>
+                <div class="button_panel">
+                    // A button to send the Increment message
+                    <div>
+                        <button class="button" onclick={ctx.link().callback(|_| GameAction{entity: Plyr::P1, action: Msg::Increment})}>
+                            { "+1 to P1" }
+                        </button>
 
-                    // // A button to send the Decrement message
-                    // <button onclick={ctx.link().callback(|_| GameAction{entity: Plyr::Table, action: Msg::Decrement})}>
-                    //     { "-1 to table" }
-                    // </button>
+                        // A button to send the Decrement message
+                        <button onclick={ctx.link().callback(|_| GameAction{entity: Plyr::P1, action: Msg::Decrement})}>
+                            { "-1 to P1" }
+                        </button>
+                    </div>
 
                     // A button to send the Increment message
-                    <button class="button" onclick={ctx.link().callback(|_| GameAction{entity: Plyr::P1, action: Msg::Increment})}>
-                        { "+1 to P1" }
-                    </button>
-
-                    // A button to send the Decrement message
-                    <button onclick={ctx.link().callback(|_| GameAction{entity: Plyr::P1, action: Msg::Decrement})}>
-                        { "-1 to P1" }
-                    </button>
-
-                    // A button to send the Increment message
-                    <button class="button" onclick={ctx.link().callback(|_| GameAction{entity: Plyr::P2, action: Msg::Increment})}>
-                        { "+1 to P2" }
-                    </button>
-
-                    // A button to send the Decrement message
-                    <button onclick={ctx.link().callback(|_| GameAction{entity: Plyr::P2, action: Msg::Decrement})}>
-                        { "-1 to P2" }
-                    </button>
+                    <div>
+                        <button class="button" onclick={ctx.link().callback(|_| GameAction{entity: Plyr::P2, action: Msg::Increment})}>
+                            { "+1 to P2" }
+                        </button>
+                        <button onclick={ctx.link().callback(|_| GameAction{entity: Plyr::P2, action: Msg::Decrement})}>
+                            { "-1 to P2" }
+                        </button>
+                    </div>
 
                     // A button to send the Increment message
-                    <button class="button" onclick={ctx.link().callback(|_| GameAction{entity: Plyr::Dead, action: Msg::Increment})}>
-                        { "+1 to Dead" }
-                    </button>
+                    <div>
+                        <button class="button" onclick={ctx.link().callback(|_| GameAction{entity: Plyr::Dead, action: Msg::Increment})}>
+                            { "+1 to Dead" }
+                        </button>
+                        <button onclick={ctx.link().callback(|_| GameAction{entity: Plyr::Dead, action: Msg::Decrement})}>
+                            { "-1 to Dead" }
+                        </button>
+                    </div>
 
-                    // A button to send the Decrement message
-                    <button onclick={ctx.link().callback(|_| GameAction{entity: Plyr::Dead, action: Msg::Decrement})}>
-                        { "-1 to Dead" }
-                    </button>
+                    // A button to send the Increment message
+                    <div>
+                        <button class="button" onclick={ctx.link().callback(|_| GameAction{entity: Plyr::Inning, action: Msg::Increment})}>
+                            { "+1 to Inning" }
+                        </button>
+                        <button onclick={ctx.link().callback(|_| GameAction{entity: Plyr::Inning, action: Msg::Decrement})}>
+                            { "-1 to Inning" }
+                        </button>
+                    </div>
+
+                    // A button to send the Increment message
+                    <div>
+                        <button class="button" onclick={ctx.link().callback(|_| GameAction{entity: Plyr::Rack, action: Msg::Increment})}>
+                            { "+1 to Rack" }
+                        </button>
+                        <button onclick={ctx.link().callback(|_| GameAction{entity: Plyr::Rack, action: Msg::Decrement})}>
+                            { "-1 to Rack" }
+                        </button>
+                    </div>
 
                 </div>
-
-                // Display the current value of the counter
-                <p class="counter">
-                   { self.table }
-                </p>
-                <p class="counter">
-                { self.player1 }
-             </p>
-             <p class="counter">
-             { self.player2 }
-          </p>
-          <p class="counter">
-          { self.dead }
-       </p>
-
-
+                <div class="score_panel">
+                    <table style="width:100%">
+                        <tr>
+                            <th/>
+                            <th>{"Player 1"}</th>
+                            <th>{"Innings"}</th>
+                            <th>{"Dead"}</th>
+                            <th>{"Player 2"}</th>
+                        </tr>
+                        <tr>
+                            <td>{"Totals"}</td>
+                            <td>{self.totals.player1}</td>
+                            <td>{self.totals.innings}</td>
+                            <td>{self.totals.dead}</td>
+                            <td>{self.totals.player2}</td>
+                        </tr>
+                        <tr>
+                            <td>{format!("Rack {0}", self.rack)}</td>
+                            <td>{self.racks[self.rack].player1}</td>
+                            <td>{self.racks[self.rack].innings}</td>
+                            <td>{self.racks[self.rack].dead}</td>
+                            <td>{self.racks[self.rack].player2}</td>
+                        </tr>
+                    </table>
+                </div>
             </div>
         }
     }
 }
+
+fn update_totals(racks: &Vec<Rack>) -> Rack {
+    let mut player1: i64 = 0;
+    let mut player2: i64 = 0;
+    let mut innings: i64 = 0;
+    let mut dead: i64 = 0;
+    for i in 0..racks.len() {
+        player1 += racks[i].player1;
+        player2 += racks[i].player2;
+        innings += racks[i].innings;
+        dead += racks[i].dead;
+    }
+    return Rack{ player1, player2, innings, dead }
+}
+
 
 fn main() {
     yew::Renderer::<App>::new().render();
